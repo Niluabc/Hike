b8eddd112ae71476e99b8a8496b729ec50dd4e46
dhaval512 <dhavalshah512@gmail.com>
2023-11-05 10:58:24 +0530

    Initial Commit

---
+++ Hike/HikeApp.swift
@@ -0,0 +1,17 @@
+//
+//  HikeApp.swift
+//  Hike
+//
+//  Created by Nilam Shah on 05/11/23.
+//
+
+import SwiftUI
+
+@main
+struct HikeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
